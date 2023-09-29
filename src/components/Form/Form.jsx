8eb26da8122fb466aff220a4079fff8a3a0e1a2d
YuliaPysarenko <y.pisarenko@ukr.net>
2023-09-29 17:50:52 +0300

    work

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -52,7 +52,7 @@ class Form extends Component {
               value={this.state.name}
               type="text"
               name="name"
-              // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
               onChange={this.handleInputContacts} />
@@ -65,7 +65,7 @@ class Form extends Component {
               value={this.state.number}
               type="tel"
               name="number"
-              // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
               onChange={this.handleInputContacts}
@@ -88,7 +88,7 @@ class Form extends Component {
           //   onChange={this.handleInputContacts}/>  */}
            
            <ul>
-           <li key={this.state.name}>{this.state.value}
+           <li key={this.state.name}>{this.state.name} {this.state.number}
          </li>
           </ul> 
 {/* 
