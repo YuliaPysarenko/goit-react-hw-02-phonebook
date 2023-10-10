0d559c509b7bf529f04dec11622faee3e8b561f7
YuliaPysarenko <y.pisarenko@ukr.net>
2023-10-10 10:46:04 +0300

    HomeWork

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -80,24 +80,4 @@ class App extends Component {
 
 }
 
-
-
-// export const App = () => {
-//   return (
-   
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101'
-    //   }}
-    // >
-    //   React homework template
-    // </div>
-//   );
-// };
-
 export default App
